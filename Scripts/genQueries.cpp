1643845edf511b0b56d3680131f627a86771b835
Rodrigo Rivera <rodrivera@udec.cl>
2017-06-19 18:40:16 -0400

    Trajectories for San Francisco generated and added.

--- Scripts/genQueries.cpp
+++ Scripts/genQueries.cpp
@@ -9,19 +9,25 @@ int main(int argc, char const *argv[])
 
 	if (argc < 5){
 		cout << "Usage: ./gen num_queries percX percY percT" << endl;
-		cout << "percX, percY and percT must be float numbers between 0 and 1 (inclusive)." << endl;
+		cout << "percX, percY and percT must be float numbers between 0 and 100 (inclusive)." << endl;
 		return -1;
 	}
 
-	//int minX = 281, maxX = 2854; int minY = 3935, maxY = 30851; // Oldenburg
-	int minX = 56977411, maxX = 58790663; int minY = 51682794, maxY = 53102345; // San Francisco
+	//int minX = 281, maxX = 2854; int minY = 3935, maxY = 30851; 					// Oldenburg
+	int minX = 56977411, maxX = 58790663; int minY = 51682794, maxY = 53102345; 	// San Francisco
+
 	double minT = 1.0, maxT = 100.0;
 
 	int dX = maxX-minX, dY = maxY-minY;
 	double dT = maxT-minT;
 
 	int num = atoi(argv[1]);
-	float percX = atof(argv[2]), percY = atof(argv[3]), percT = atof(argv[4]);
+	float percX = atof(argv[2])/100, percY = atof(argv[3])/100, percT = atof(argv[4])/100;
+
+	if ( percX < 0 || percX > 100 || percY < 0 || percY > 100 || percT < 0 || percT > 100){
+		cout << "percX, percY and percT must be float numbers between 0 and 100 (inclusive)." << endl;
+		return -1;
+	}
 
 	srand(time(NULL));
 
--- Scripts/testAll.sh
+++ Scripts/testAll.sh
@@ -2,23 +2,26 @@
 AUX=""
 #AUX="../"
 
-network="$AUX""data/Oldenburg/nodes.txt ""$AUX""data/Oldenburg/edges.txt"
+#city="Oldenburg"
+city="$4"
+
+network="$AUX""data/""$city""/nodes.txt ""$AUX""data/""$city""/edges.txt"
 qFile="$1""_""$2""_""$3"
-queries="$AUX""data/Oldenburg/queries/""$qFile"".txt"
+queries="$AUX""data/""$city""/queries/""$qFile"".txt"
 
 if [ ! -f "$queries" ]; then
     echo "Queries file not found!"
-    echo "Usage: testAll.sh percX percY percT"
-    echo "with perc values between 0 and 1"
+    echo "Usage: testAll.sh percX percY percT cityname"
+    echo "with perc values between 0 and 100"
     exit
 fi
 
 declare -a SUFFIXES=("200" "400" "800" "1200" "1600" "2000")
 for N in "${SUFFIXES[@]}"
 do
-	trajectories="$AUX""data/Oldenburg/trajectories/""$N"".dat"
-	outFNR="$AUX""data/Oldenburg/output/FNR/""$qFile""-""$N"".txt"
-	outX="$AUX""data/Oldenburg/output/X/""$qFile""-""$N"".txt"
+	trajectories="$AUX""data/""$city""/trajectories/""$N"".dat"
+	outFNR="$AUX""data/""$city""/output/FNR/""$qFile""-""$N"".txt"
+	outX="$AUX""data/""$city""/output/X/""$qFile""-""$N"".txt"
 	echo "Executing.. FNR with ""$N"" objects.."
 	./"$AUX"fnrtest.out $network $trajectories $queries $outFNR
 	echo "Executing.. X with ""$N"" objects.."
