59aaebad55ef5cb6617fb5acde905d53a7386189
Rodrigo Rivera <rodrivera@udec.cl>
2017-06-22 21:03:30 -0400

    Added clean script to remove output test files

---
+++ Scripts/clean.sh
@@ -0,0 +1,7 @@
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+rm -f "$SCRIPT_DIR"/../data/Oldenburg/output/X/*
+rm -f "$SCRIPT_DIR"/../data/Oldenburg/output/FNR/*
+
+rm -f "$SCRIPT_DIR"/../data/San_Francisco/output/X/*
+rm -f "$SCRIPT_DIR"/../data/San_Francisco/output/FNR/*
